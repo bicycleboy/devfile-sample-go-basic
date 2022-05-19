5a4f4efd5ca1fb19e9cfe9c9d580d04b81335b46
Chris <cneedham@redhat.com>
2022-05-19 13:31:31 +1000

    cruel

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello Cruel World, %s!", r.URL.Path[1:])
 }
